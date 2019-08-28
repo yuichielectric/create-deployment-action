4a791173183e2e4dd1f937ee05450924031ad986
Yuichi Tanaka <yuichielectric@github.com>
2019-08-28 22:33:23 +0900

    Implement creating deployment action

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,22 @@
 import * as core from '@actions/core';
+import * as github from '@actions/github';
+import * as path from 'path';
 
 async function run() {
   try {
-    const myInput = core.getInput('myInput');
-    core.debug(`Hello ${myInput}`);
+    const ref = core.getInput('ref') || process.env['GITHUB_SHA'];
+    const environment = core.getInput('environment');
+    const repository = process.env['GITHUB_REPOSITORY'];
+    if (repository != undefined) {
+      const owner = path.dirname(repository);
+      const repo = path.basename(repository);
+      github.repos.createDeployment({
+        owner,
+        repo,
+        ref
+      });
+    }
+    core.debug(`Hello ${ref}`);
   } catch (error) {
     core.setFailed(error.message);
   }
