0059fd71cd57a7e75f6c288e61ba42ef5816eb1d
Yuichi Tanaka <yuichielectric@github.com>
2019-09-16 12:09:40 -0700

    Refactor

--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,23 @@
-import * as core from '@actions/core';
-import * as github from '@actions/github';
+import * as core from "@actions/core";
+import * as github from "@actions/github";
 
 async function run() {
+  const token = process.env["GITHUB_TOKEN"];
+  if (token == undefined) {
+    core.setFailed("GitHub token is needed.");
+    return;
+  }
+  const octokit = new github.GitHub(token);
+  const context = github.context;
+  const ref = core.getInput("ref", { required: true });
+  const payload = {
+    ...context.repo,
+    ref: ref,
+    required_contexts: []
+  };
   try {
-    core.debug('Start processing');
-    const token = process.env['GITHUB_TOKEN'];
-    core.debug(`token: ${token}`);
-    if (token == undefined) {
-      return;
-    }
-    const octokit = new github.GitHub(token);
-    const context = github.context;
-    const ref = core.getInput('ref') || process.env['GITHUB_SHA'];
-    core.debug(`ref: ${ref}`);
-    if (ref == undefined) {
-      return;
-    }
-    const environment = core.getInput('environment');
-    const payload = {
-      ...context.repo,
-      ref: ref,
-      required_contexts: []
-    };
-    core.debug(JSON.stringify(payload));
     const result = await octokit.repos.createDeployment(payload);
-    core.debug(JSON.stringify(result));
-    core.setOutput("deployment-id", result['data']['id']);
+    core.setOutput("deployment-id", result["data"]["id"]);
   } catch (error) {
     core.setFailed(error.message);
   }
